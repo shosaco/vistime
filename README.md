ee1b202912fed0b9a5f74b24421e31b3c2c85cb0
sandro <sandro.raabe@d-fine.de>
2017-01-29 11:52:56 +0100

    init changes

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
 # vistime
 an R package for pretty timeline creation
 
-Given a data frame containing Events, Groups (optional), StartDates, EndDates (optional) and colors (optional), this creates a `plotly` object with the events arranged nicely. It distinguishes between single events (having StartDate == EndDate) and ranges (otherwise). The `plotly` object can be modified further using `plotly_build`. It can be used in a Shiny App via `plotlyOutput` and `renderPlotly`. Date Format can be `String`, `Date` or `POSIXct` in standard notation.
+`vistime` is the first R package to create _fully interactive_ timelines/Gantt charts using Plotly.js. The charts can be included in Shiny apps and manipulated via `plotly_build()`.
+
+Given a data frame containing Event Names and Dates (can be `String`, `Date` or `POSIXct` in standard notation) and optionally Groups, EndDates for ranges and colors, it creates a charming diagram of the given Dates and Events. It distinguishes between single events (having StartDate == EndDate) and ranges (given EndDate). Colors can be handed over as the `color`-argument, if none are given, they are chosen from `RColorBrewer`.
 
 **Feedback welcome:** shosaco_nospam@hotmail.com  
 
 ******
 ## example 1:
 ```{r}
-dat <- data.frame(Room=c("Room 1","Room 2","Room 3"),
-                  Language=c("English", "German", "French"),
-                  start=as.POSIXct(c("2014-03-14 14:00",
-                                     "2014-03-14 15:00",
-                                     "2014-03-14 14:30")),
-                  end=as.POSIXct(c("2014-03-14 15:00",
-                                   "2014-03-14 16:00",
-                                   "2014-03-14 15:30")))
-vistime(dat, start="start", end="end", groups="Room", events="Language")
+dat <- data.frame(Room = c("Room 1", "Room 2", "Room 3"),
+                  Language = c("English", "German", "French"),
+                  start = c("2017-03-14 14:00"," 2017-03-14 15:00", "2017-03-14 14:30"),
+                  end = c("2017-03-14 15:00", "2017-03-14 16:00", "2017-03-14 15:30"))
+vistime(dat, events="Language", groups="Room")
 ```
 ![](examples/ex1.png)
  
@@ -24,36 +22,16 @@ vistime(dat, start="start", end="end", groups="Room", events="Language")
 ## example 2:
 ```{r}
 dataGroups <- data.frame(
-  content = c("Open", "Open",
-              "Open", "Open", "Half price entry",
-              "Staff meeting", "Open", "Adults only", "Open", "Hot tub closes",
-              "Siesta"),
-  start = c("2016-05-01 07:30:00", "2016-05-01 14:00:00",
-            "2016-05-01 06:00:00", "2016-05-01 14:00:00", "2016-05-01 08:00:00",
-            "2016-05-01 08:00:00", "2016-05-01 08:30:00", "2016-05-01 14:00:00",
-            "2016-05-01 16:00:00", "2016-05-01 19:30:00",
-            "2016-05-01 12:00:00"),
-  end   = c("2016-05-01 12:00:00", "2016-05-01 20:00:00",
-            "2016-05-01 12:00:00", "2016-05-01 22:00:00", "2016-05-01 10:00:00",
-            "2016-05-01 08:30:00", "2016-05-01 12:00:00", "2016-05-01 16:00:00",
-            "2016-05-01 20:00:00", NA,
-            "2016-05-01 14:00:00"),
-  group = c(rep("lib", 2), rep("gym", 3), rep("pool", 5), "siesta"))
+  content = c("Open", "Open", "Open", "Open", "Half price entry", "Staff meeting", "Open", "Adults only", "Open", "Hot tub closes"),
+  start = c("2017-05-01 07:30:00", "2017-05-01 14:00:00", "2017-05-01 06:00:00", "2017-05-01 14:00:00", "2017-05-01 08:00:00",
+            "2017-05-01 08:00:00", "2017-05-01 08:30:00", "2017-05-01 14:00:00","2017-05-01 16:00:00", "2017-05-01 19:30:00"),
+  end   = c("2017-05-01 12:00:00", "2017-05-01 20:00:00", "2017-05-01 12:00:00", "2017-05-01 22:00:00", "2017-05-01 10:00:00",
+            "2017-05-01 08:30:00", "2017-05-01 12:00:00", "2017-05-01 16:00:00", "2017-05-01 20:00:00", NA),
+  group = c(rep("Tennis Court", 2), rep("Billard", 3), rep("Pool", 5)))
 
-vistime(dataGroups, start="start", end="end", groups="group", events="content")
+vistime(dataGroups, events="content")
 ```
 ![](examples/ex2.png)
 
 
-## example 3:
-```{r}
-library(timeline)
-data(ww2)
-ww2.events$EndDate <- ww2.events$Date
-ww2.events$StartDate <- ww2.events$Date
-names(ww2.events)<-c("Person", "Date", "Group", "EndDate", "StartDate")
-ww2.events<- ww2.events[,names(ww2)]
-vistime(rbind(ww2, ww2.events), events="Person")
-```
-![](examples/ex3.png)
 
--- vistime.R
+++ vistime.R
@@ -5,71 +5,57 @@ library(RColorBrewer)
 
 #########################
 #ex1:
-dat <- data.frame(Room=c("Room 1","Room 2","Room 3"),
-                  Language=c("English", "German", "French"),
-                  start=as.POSIXct(c("2014-03-14 14:00",
-                                     "2014-03-14 15:00",
-                                     "2014-03-14 14:30")),
-                  end=as.POSIXct(c("2014-03-14 15:00",
-                                   "2014-03-14 16:00",
-                                   "2014-03-14 15:30")))
-# vistime(dat, start="start", end="end", groups="Room", events="Language")
+dat <- data.frame(Room = c("Room 1", "Room 2", "Room 3"),
+                  Language = c("English", "German", "French"),
+                  start = c("2017-03-14 14:00"," 2017-03-14 15:00", "2017-03-14 14:30"),
+                  end = c("2017-03-14 15:00", "2017-03-14 16:00", "2017-03-14 15:30"))
+vistime(dat, events="Language", groups="Room")
 
 #################################################
 
 ##################
 # ex2:
-# dataGroups <- data.frame(
-#   content = c("Open", "Open",
-#               "Open", "Open", "Half price entry",
-#               "Staff meeting", "Open", "Adults only", "Open", "Hot tub closes",
-#               "Siesta"),
-#   start = c("2016-05-01 07:30:00", "2016-05-01 14:00:00",
-#             "2016-05-01 06:00:00", "2016-05-01 14:00:00", "2016-05-01 08:00:00",
-#             "2016-05-01 08:00:00", "2016-05-01 08:30:00", "2016-05-01 14:00:00",
-#             "2016-05-01 16:00:00", "2016-05-01 19:30:00",
-#             "2016-05-01 12:00:00"),
-#   end   = c("2016-05-01 12:00:00", "2016-05-01 20:00:00",
-#             "2016-05-01 12:00:00", "2016-05-01 22:00:00", "2016-05-01 10:00:00",
-#             "2016-05-01 08:30:00", "2016-05-01 12:00:00", "2016-05-01 16:00:00",
-#             "2016-05-01 20:00:00", NA,
-#             "2016-05-01 14:00:00"),
-#   group = c(rep("lib", 2), rep("gym", 3), rep("pool", 5), "siesta"))
-# 
-# vistime(dataGroups, start="start", end="end", groups="group", events="content")
+dataGroups <- data.frame(
+  content = c("Open", "Open", "Open", "Open", "Half price entry", "Staff meeting", "Open", "Adults only", "Open", "Hot tub closes"),
+  start = c("2017-05-01 07:30:00", "2017-05-01 14:00:00", "2017-05-01 06:00:00", "2017-05-01 14:00:00", "2017-05-01 08:00:00",
+            "2017-05-01 08:00:00", "2017-05-01 08:30:00", "2017-05-01 14:00:00","2017-05-01 16:00:00", "2017-05-01 19:30:00"),
+  end   = c("2017-05-01 12:00:00", "2017-05-01 20:00:00", "2017-05-01 12:00:00", "2017-05-01 22:00:00", "2017-05-01 10:00:00",
+            "2017-05-01 08:30:00", "2017-05-01 12:00:00", "2017-05-01 16:00:00", "2017-05-01 20:00:00", NA),
+  group = c(rep("Tennis Court", 2), rep("Billard", 3), rep("Pool", 5)))
+
+vistime(dataGroups, events="content")
 
 ############################
 # ex.3
 # library(timeline)
 # data(ww2)
-# ww2.events$EndDate <- ww2.events$Date
-# ww2.events$StartDate <- ww2.events$Date
-# names(ww2.events)<-c("Person", "Date", "Group", "EndDate", "StartDate")
+# ww2.events$end <- ww2.events$Date
+# ww2.events$start <- ww2.events$Date
+# names(ww2.events)<-c("Person", "Date", "Group", "end", "start")
 # ww2.events<- ww2.events[,names(ww2)]
-# vistime(rbind(ww2, ww2.events), events="Person")
+# vistime(rbind(ww2, ww2.events), events="Person", start="StartDate", end="EndDate")
 
-
-vistime <- function(data, start="StartDate", end="EndDate", groups="Group", events="Event", colors=NULL){
+vistime <- function(data, start="start", end="end", groups="group", events="event", colors=NULL){
   
-  if(! groups %in% names(data)) data$Group <- ""
-  if(! end %in% names(data)) data$EndDate <- data[, start]
-  names(data)[names(data)==start] <- "StartDate"
-  names(data)[names(data)==end] <- "EndDate"
-  names(data)[names(data)==groups] <- "Group"
-  names(data)[names(data)==events] <- "Event"
+  if(! groups %in% names(data)) data$group <- ""
+  if(! end %in% names(data)) data$end <- data[, start]
+  names(data)[names(data)==start] <- "start"
+  names(data)[names(data)==end] <- "end"
+  names(data)[names(data)==groups] <- "group"
+  names(data)[names(data)==events] <- "event"
   
   # sort out the classes
-  data$StartDate <- as.POSIXct(data$StartDate)
-  data$EndDate <- as.POSIXct(data$EndDate)
-  data[, c("Event", "Group")] <- sapply(data[, c("Event", "Group")], as.character)
+  data$start <- as.POSIXct(data$start)
+  data$end <- as.POSIXct(data$end)
+  data[, c("event", "group")] <- sapply(data[, c("event", "group")], as.character)
   
-  # fix missing EndDates for events
-  if(any(is.na(data$EndDate))) data$EndDate[is.na(data$EndDate)] <- data$StartDate[is.na(data$EndDate)]
+  # fix missing ends for events
+  if(any(is.na(data$end))) data$end[is.na(data$end)] <- data$start[is.na(data$end)]
 
   # set the tooltips
-  data$Tooltip <- ifelse(data$StartDate == data$EndDate,
-                         paste0("<b>",data$Event,": ",data$StartDate,"</b>"),
-                         paste0("<b>",data$Event,":</b> from <b>",data$StartDate,"</b> to <b>",data$EndDate,"</b>"))
+  data$tooltip <- ifelse(data$start == data$end,
+                         paste0("<b>",data$event,": ",data$start,"</b>"),
+                         paste0("<b>",data$event,":</b> from <b>",data$start,"</b> to <b>",data$end,"</b>"))
   
   # set the colors
   if(is.null(colors)){
@@ -83,7 +69,7 @@ vistime <- function(data, start="StartDate", end="EndDate", groups="Group", even
   #  1. Determine the correct subplot for each event                ######
   ########################################################################
   
-  data$subplot <- as.numeric(as.factor(data$Group))
+  data$subplot <- as.numeric(as.factor(data$group))
   
   
   ########################################################################
@@ -92,21 +78,21 @@ vistime <- function(data, start="StartDate", end="EndDate", groups="Group", even
   for(sp in unique(data$subplot)){
     next.y <- 1
     
-    # subset data for this Group
+    # subset data for this group
     thisData <- subset(data, subplot == sp)
   
     for(row in (1:nrow(thisData))){
       toAdd <- thisData[row,]
       
       # for events: set on new level if it occurs on the same day as previous
-      if(toAdd$StartDate == toAdd$EndDate){
-        if(row>1 && toAdd$StartDate == thisData[row-1, "StartDate"]){
+      if(toAdd$start == toAdd$end){
+        if(row>1 && toAdd$start == thisData[row-1, "start"]){
           next.y <- next.y + 1
         }else{
           next.y <- 1
         }
       # for ranges: if this event starts before previous ends, set on new y level (up or below)
-      }else if(row>1 && toAdd$StartDate < thisData[row-1, "EndDate"]){
+      }else if(row>1 && toAdd$start < thisData[row-1, "end"]){
         if(next.y == 2) next.y <- 1
         else next.y <- next.y + 1
       }else{
@@ -121,20 +107,20 @@ vistime <- function(data, start="StartDate", end="EndDate", groups="Group", even
   #  3. Set "intelligent" labels for events                           #######
   ###########################################################################
   
-  # data$labelPos <- ifelse(data$StartDate == data$EndDate, 
-  #                         rep(c("top", "bottom"), ceiling(nrow(data[data$StartDate == data$EndDate,])/2)),
+  # data$labelPos <- ifelse(data$start == data$end, 
+  #                         rep(c("top", "bottom"), ceiling(nrow(data[data$start == data$end,])/2)),
   #                         "center")
   data$labelPos <- "center"
   
-  data$label <- ifelse(data$StartDate == data$EndDate, 
-                       ifelse(nchar(data$Event) > 10, paste0(substr(data$Event, 1, 8), "..."), data$Event),
-                       data$Event)
+  data$label <- ifelse(data$start == data$end, 
+                       ifelse(nchar(data$event) > 10, paste0(substr(data$event, 1, 8), "..."), data$event),
+                       data$event)
   
   #############################################################################
   #  4. set interval for vertical lines                                   #####
   #############################################################################  
   
-  total_range <- difftime(max(data$EndDate), min(data$StartDate), units="secs")
+  total_range <- difftime(max(data$end), min(data$start), units="secs")
   if(total_range <= 60*60){ # max 1 hour
     interval <- 60*10 # 10-min-intervals
   }else if(total_range < 60*60*24){ # max 1 day
@@ -150,18 +136,18 @@ vistime <- function(data, start="StartDate", end="EndDate", groups="Group", even
   #  
   #############################################################################
   
-  rangeNumbers <- unique(subset(data, StartDate != EndDate)$subplot)
+  rangeNumbers <- unique(subset(data, start != end)$subplot)
   ranges <- lapply(rangeNumbers, function(sp) {
     next.y <- 1
     
-    # subset data for this Group
-    thisData <- subset(data, StartDate != EndDate & subplot == sp)
+    # subset data for this group
+    thisData <- subset(data, start != end & subplot == sp)
     maxY <- max(thisData$y) + 1
   
     p <- plot_ly(type = "scatter", mode="lines") 
     
     # 1. add vertical line for each year/day
-    for(day in seq(min(data$StartDate), max(data$EndDate), interval)){
+    for(day in seq(min(data$start), max(data$end), interval)){
       p <- add_trace(p, x = as.POSIXct(day, origin="1970-01-01"), y= c(0, maxY), mode = "lines", 
                      line=list(color = toRGB("grey90")), showlegend=F, hoverinfo="none")
     }
@@ -172,13 +158,13 @@ vistime <- function(data, start="StartDate", end="EndDate", groups="Group", even
   
       p <- add_trace(p,
                      mode="lines",
-                     x = c(toAdd$StartDate, toAdd$EndDate),  # von, bis
+                     x = c(toAdd$start, toAdd$end),  # von, bis
                      y = toAdd$y,
                      line = list(color = toAdd$col, width = 20),
                      showlegend = F,
                      hoverinfo="text", 
-                     text=toAdd$Tooltip) %>%
-        add_text(x = toAdd$StartDate + (toAdd$EndDate-toAdd$StartDate)/2,  # in der Mitte
+                     text=toAdd$tooltip) %>%
+        add_text(x = toAdd$start + (toAdd$end-toAdd$start)/2,  # in der Mitte
                  y = toAdd$y,
                  textfont = list(family = "sans serif", size = 14, color = toRGB("black")),
                  textposition = "center",
@@ -188,15 +174,15 @@ vistime <- function(data, start="StartDate", end="EndDate", groups="Group", even
     }
     
     return(p %>% layout(hovermode = 'closest',
-                    margin = list(l=100),
+                    margin = list(l=max(nchar(data$event)) * 10),
                     # Axis options:
                     # 1. Remove gridlines
-                    # 2. Customize y-axis tick labels and show Group names instead of numbers
+                    # 2. Customize y-axis tick labels and show group names instead of numbers
                     xaxis = list(showgrid = F, title = ''), 
                     yaxis = list(showgrid = F, title = '',
                                  tickmode = "array", tickvals = 1:maxY,
                                  ticktext = c(rep("", (maxY-1)/2), # Leerzeilen
-                                              as.character(toAdd$Group), # Group name in the center
+                                              as.character(toAdd$group), # group name in the center
                                               rep("", (maxY+1)/2)) # Leerzeilen
                     )))
   })
@@ -207,10 +193,10 @@ vistime <- function(data, start="StartDate", end="EndDate", groups="Group", even
   #
   #######################################################################
   
-  eventNumbers <- unique(subset(data, StartDate == EndDate)$subplot)
+  eventNumbers <- unique(subset(data, start == end)$subplot)
   events <- lapply(eventNumbers, function(sp) {
     # subset data for this Category
-    thisData <- subset(data, StartDate == EndDate & subplot == sp)
+    thisData <- subset(data, start == end & subplot == sp)
     maxY <- max(thisData$y) + 1
   
     # add vertical lines to plot
@@ -218,29 +204,29 @@ vistime <- function(data, start="StartDate", end="EndDate", groups="Group", even
     
     # 1. add vertical line for each year/day
 
-    for(day in seq(min(data$StartDate), max(data$EndDate), interval)){
+    for(day in seq(min(data$start), max(data$end), interval)){
       p <- add_lines(p, x = as.POSIXct(day, origin="1970-01-01"), y= c(0, maxY),
                      line=list(color = toRGB("grey90")), showlegend=F, hoverinfo="none")
     }
   
     # add all the markers for this Category
-    p <- add_markers(p, x=~StartDate, y=~y,
+    p <- add_markers(p, x=~start, y=~y,
                         marker = list(color = ~col, size=15,
                                       line = list(color = 'black', width = 1)),
-                        showlegend = F, hoverinfo="text", text=~Tooltip)
+                        showlegend = F, hoverinfo="text", text=~tooltip)
   
     # add annotations
-    p <- add_text(p, x=~StartDate, y=~y, textfont = list(family = "sans serif", size = 14, color = toRGB("black")),
+    p <- add_text(p, x=~start, y=~y, textfont = list(family = "sans serif", size = 14, color = toRGB("black")),
                   textposition = ~labelPos, showlegend=F, text = ~label, hoverinfo="none")
   
     # fix layout
     p <-  layout(p, hovermode = 'closest',
-                    margin = list(l = 200),
+                 margin = list(l=max(nchar(data$event)) * 10),
                     xaxis = list(showgrid = F, title=''),
                      yaxis = list(showgrid = F, title = '',
                               tickmode = "array", tickvals = 1:maxY,
                               ticktext = c(rep("", (maxY-1)/2), # Leerzeilen
-                                           thisData$Group[1], # Group name in the center
+                                           thisData$group[1], # group name in the center
                                            rep("", (maxY+1)/2)) # Leerzeilen
                   ))
   })
