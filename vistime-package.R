050e052afcbc5e8db8e10b67f31e6ee18a256a57
Sandro Raabe <shosaco@users.noreply.github.com>
2020-04-17 22:01:19 +0200

    vistime-package doc

---
+++ vistime-package.R
@@ -0,0 +1,8 @@
+#' Pretty timelines.
+#' 
+#' @name vistime-package
+#' @docType package
+#' @title vistime
+#' @author Sandro Raabe \email{sa.ra.online@posteo.de}
+#' @keywords timeline gantt timelines vistime plotly posix ggplot2
+NULL
